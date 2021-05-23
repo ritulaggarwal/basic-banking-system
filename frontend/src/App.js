f34b6923089e5a37a6b47b7e1d79a2069d1b02ff
Ritul Aggarwal <ritul.aggarwal2000@gmail.com>
2021-05-23 18:55:44 +0530

    React-Bootstrap, Header,Footer Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import './bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-const App=()=> {
+
+const App = () => {
   return (
     <>
-        <h1>Welcome to Bank</h1>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Welcome to Bank</h1>
+        </main>
+
+      </Container>
+
+      <Footer />
+
     </>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Navbar, Nav, Container} from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                <Navbar.Brand href="/">Bank System</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/allCustomer"><i className="fas fa-user"></i> View All Customers</Nav.Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
