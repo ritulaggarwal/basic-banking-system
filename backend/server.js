3a4427655b4f5298af5036fdadfb816a07ed6908
Ritul Aggarwal <ritul.aggarwal2000@gmail.com>
2021-06-04 03:29:37 +0530

    Prepare For Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -13,12 +14,26 @@ const app=express()
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Api..')
-})
+// app.get('/',(req,res)=>{
+//     res.send('Api..')
+// })
 
 app.use('/api/customers',customerRoutes)
 app.use('/api/transactions',transactionRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 
 const PORT=process.env.PORT || 5000
